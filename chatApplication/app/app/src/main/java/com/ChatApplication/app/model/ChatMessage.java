f63505529781cba302e1342d34c55a6e5bc47093
Chaitanya Kashyap <121999859+chaitanya19Gla@users.noreply.github.com>
2026-06-02 18:04:40 +0530

    Add files via upload

---
+++ chatApplication/app/app/src/main/java/com/ChatApplication/app/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.ChatApplication.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+
+
+
+}
